5bbf742d584c1a0ba1fb4c3ea3e174d56c1008ca
Matthew Sherman <msherman0801@gmail.com>
2019-04-03 08:46:23 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,14 @@
 module SongsHelper
 
+    def last_updated(post)
+        post.updated_at.strftime("Last updated %A, %b %e, at %l:%M %p")
+    end
+
+    def display_artist(song)
+        if song.artist
+            link_to song.artist_name, artist_path(song.artist)
+        else
+            link_to "Add Artist", edit_song_path(song)
+        end
+    end
 end
