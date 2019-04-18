37019ff72a6b46ab4f4d8d38ffaf3da6e6324925
Johnathon Wood <jaywood128@gmail.com>
2019-04-18 18:43:43 -0400

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,11 @@
 module ArtistsHelper
+
+    def display_artist(song)
+        
+        if song.artist
+            link_to(song.artist.name, artist_path(song.artist))
+        else 
+            link_to(song.name, edit_artist(song))
+        end
+    end
 end
