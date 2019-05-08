9f822671dd645f69d1744ac80e24f1487f2fd843
Jane Venter <janeanneventer@gmail.com>
2019-05-07 17:41:14 -0700

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,10 @@
 module SongsHelper
-
+    #def display_artist 
+        #binding.pry
+        #if self.artist_url 
+            #link_to "#{self.artist.name}", artist_path(self.artist)
+        #else 
+            #link_to "Add Artist", song_edit_path(self.id) 
+        #end   
+    #end
 end
